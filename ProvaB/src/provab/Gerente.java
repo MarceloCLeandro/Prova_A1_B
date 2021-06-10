535bd8cfb887bc4f5ba406dba10597a953014d4d
MarceloCLeandro <65774706+MarceloCLeandro@users.noreply.github.com>
2021-06-09 22:18:20 -0300

    Dando continuação
    Dando andamento na prova

--- ProvaB/src/provab/Gerente.java
+++ ProvaB/src/provab/Gerente.java
@@ -9,6 +9,73 @@ package provab;
  *
  * @author cleber
  */
-public class Gerente {
-    
+public class Gerente extends Trabalhador {
+
+    private String departamento, regiao[];
+    private float bonificacao;
+
+    public Gerente(String departamento, String[] regiao, float bonificacao) {
+        this.departamento = departamento;
+        this.regiao = regiao;
+        this.bonificacao = bonificacao;
+    }
+
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+
+    public String[] getRegiao() {
+        return regiao;
+    }
+
+    public void setRegiao(String[] regiao) {
+        this.regiao = regiao;
+    }
+
+    public float getBonificacao() {
+        return bonificacao;
+    }
+
+    public void setBonificacao(float bonificacao) {
+        this.bonificacao = bonificacao;
+    }
+
+    @Override
+    public String toString() {
+        return "Departamento Gerenciado = " + departamento
+                + ", Região Gerenciada = " + regiao
+                + ", Bonificação Anual de = " + bonificacao;
+    }
+
+    double calcularBonificacao(float salario, float bonificacao) {
+        float valorBonificacao;
+        bonificacao = (float) (15.0 / 100);
+        valorBonificacao = salario + (salario * bonificacao);
+        return valorBonificacao;
+    }
+
+    public void calcularAumento(float salario, String regiao) {
+        float aumento = 0.0f;
+        if (regiao.equalsIgnoreCase("Zona Sul")) {
+            aumento = salario + (salario * 0.10f);// calcular 10%
+        } else {
+            if (regiao.equalsIgnoreCase("C")) {
+                aumento = salario + (salario * 0.1005f);// calcular 10,05%
+            } else {
+                if (regiao.equalsIgnoreCase("VB")) {
+                    aumento = salario + (salario * 0.1085f);// calcular 10,85%
+                } else {
+                    if (regiao.equalsIgnoreCase("Go")) {
+                        aumento = salario + (salario * 0.0996f);// calcular 9,96%
+                    }
+                }
+            }
+
+        }
+
+    }
 }
--- ProvaB/src/provab/Trabalhador.java
+++ ProvaB/src/provab/Trabalhador.java
@@ -10,5 +10,45 @@ package provab;
  * @author cleber
  */
 public class Trabalhador {
-    
+
+    private String nome;
+    private int idade;
+    private float registro, salario;
+
+    public Trabalhador() {
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public float getRegistro() {
+        return registro;
+    }
+
+    public void setRegistro(float registro) {
+        this.registro = registro;
+    }
+
+    public float getSalario() {
+        return salario;
+    }
+
+    public void setSalario(float salario) {
+        this.salario = salario;
+    }
+
 }
